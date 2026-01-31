a4442ef05a91593b4b6a4c51f61a79562ee79284
Ashik gowda k <new.ashikgowda8864@gmail.com>
2026-01-31 12:03:49 +0530

    day 2

---
+++ basic problems/palindromeNumber.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class palindromeNumber {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int num = sc.nextInt();
+    int originalNumber = num;
+    int reversedNumber = 0;
+
+    if (num < 0) {
+      System.out.println("The number is not a palindrome.");
+      return;
+    }
+    while (num > 0) {
+      int digit = num % 10; // get last digit
+      reversedNumber = reversedNumber * 10 + digit; // append digit
+      num = num / 10; // remove last digit
+    }
+    if (originalNumber == reversedNumber) {
+      System.out.println("The number is a palindrome.");
+    } else {
+      System.out.println("The number is not a palindrome.");
+    }
+  }
+}
+
+// output
+// Enter a number: 121
+// The number is a palindrome.
+
+// Enter a number: 123
+// The number is not a palindrome.
---
+++ basic problems/reverseInteger.java
@@ -0,0 +1,31 @@
+public class reverseInteger {
+
+  public static void main(String[] args) {
+    int input = 151655;
+    int output = reverseInt(input);
+    System.out.println(output);
+  }
+
+  public static int reverseInt(int num) {
+    int rev = 0;
+
+    while (num != 0) {
+      int digit = num % 10;// get last digit
+      num /= 10;// remove last digit
+
+      // Check for overflow/underflow before multiplying and adding
+      if (rev > Integer.MAX_VALUE / 10 ||
+          (rev == Integer.MAX_VALUE / 10 && digit > 7)) {
+        return 0;
+      }
+      if (rev < Integer.MIN_VALUE / 10 ||
+          (rev == Integer.MIN_VALUE / 10 && digit < -8)) {
+        return 0;
+      }
+
+      rev = rev * 10 + digit;
+    }
+    return rev;
+  }
+
+}
